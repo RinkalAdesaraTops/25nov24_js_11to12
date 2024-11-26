a2e67bb225ed4651c63179deca1b4bdc924a5b2c
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-11-26 11:53:48 +0530

    variables

---
+++ operators.js
@@ -0,0 +1,23 @@
+//arithmetic operators
+// +,-,*,/,%,**,++,--
+let a = parseInt(prompt("Enter a:"))
+let b = parseInt(prompt("Enter b:"))
+// let c = a+parseInt(b)
+// document.write('<br> Addition is '+c)
+document.write('<br> Addition is '+(a+b))
+document.write('<br> Modulus is '+(a%b))
+//% - modulus - reminder
+let sqr = a**3 //a*a*a
+document.write('<br> Cube is '+sqr)
+// ++ - increment
+// -- - decrement 
+let ch = 25
+document.write('<br> ch is '+ch)
+let xy = ch++ //26 postfix
+document.write('<br> ch is '+ch)
+document.write('<br> xy is '+xy)
+
+let qr = ++ch //27 prefix
+document.write('<br> ch is '+ch)
+document.write('<br> qr is '+qr)
+
---
+++ variable.js
@@ -0,0 +1,26 @@
+//nothing
+a=10
+b=20
+c = a+b
+console.log("Addition is ",c)
+console.log("Addition plus is "+c)
+//diff b/w let var const
+//1)Var keyword - to declare variable
+//redeclare & reassign
+var p = 25
+document.write('<br> P is ',p)
+var p = "hello"
+document.write('<br> P is ',p)
+
+//2)const keyword
+//Doesnot redeclare & reassign variable
+const pi = 3.14
+document.write('<br> PI is '+pi)
+// pi = 34.5555
+// document.write('<br> PI is '+pi)
+
+//3)let - doesnot redeclare but reassign(value change) is possible
+let name1="test"
+document.write('<br> Name is '+name)
+name1=2400
+document.write('<br> Name is '+name)
