9fcaa656c208d9a10244afc2ee109eb17d0f3c08
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-11-30 11:53:56 +0530

    if else

--- operators.js
+++ operators.js
@@ -1,5 +1,6 @@
 //arithmetic operators
 // +,-,*,/,%,**,++,--
+//logical relational assignment ternary type of string op
 let a = parseInt(prompt("Enter a:"))
 let b = parseInt(prompt("Enter b:"))
 // let c = a+parseInt(b)
@@ -13,11 +14,12 @@ document.write('<br> Cube is '+sqr)
 // -- - decrement 
 let ch = 25
 document.write('<br> ch is '+ch)
-let xy = ch++ //26 postfix
+let xy = ch++ //26 postfix - get old value
 document.write('<br> ch is '+ch)
 document.write('<br> xy is '+xy)
 
-let qr = ++ch //27 prefix
+let qr = ++ch //27 prefix - get a new value
 document.write('<br> ch is '+ch)
 document.write('<br> qr is '+qr)
+
 
